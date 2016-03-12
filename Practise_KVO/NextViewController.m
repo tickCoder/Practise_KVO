14e378cc7fbba7fd3f0d0387aba8c7a9ce9ba39f
tickCoder <tickCoder@users.noreply.github.com>
2016-03-12 20:00:14 +0800

    完善一下

--- Practise_KVO/NextViewController.m
+++ Practise_KVO/NextViewController.m
@@ -30,6 +30,10 @@
 
 - (void)dealloc {
     NSLog(@"%s", __PRETTY_FUNCTION__);
+    /*
+     remove不能多次，所以要确保super没有作此操作，可以在context中作区分，比如使用唯一标识符
+     You should use -removeObserver:forKeyPath:context: instead of -removeObserver:forKeyPath: whenever possible because it allows you to more precisely specify your intent. When the same observer is registered for the same key path multiple times, but with different context pointers each time, -removeObserver:forKeyPath: has to guess at the context pointer when deciding what exactly to remove, and it can guess wrong.
+     */
     [[UserManager sharedInstance] removeObserver:self forKeyPath:@"userid" context:nil];
 }
 
@@ -46,6 +50,9 @@
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
     if ([keyPath isEqualToString:@"userid"] && object == [UserManager sharedInstance]) {
         _useridLabel.text = [NSString stringWithFormat:@"%@'s userid is %ld", [[UserManager sharedInstance] username], (long)[UserManager sharedInstance].userid];
+    } else {
+        // 若当前类无法捕捉到这个KVO，那很有可能他的super中有，若无此则会截断KVO，造成其super异常
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
     }
 }
 
--- Practise_KVO/ViewController.m
+++ Practise_KVO/ViewController.m
@@ -41,6 +41,10 @@
 }
 
 - (void)dealloc {
+    /*
+     remove不能多次，所以要确保super没有作此操作，可以在context中作区分，比如使用唯一标识符
+     You should use -removeObserver:forKeyPath:context: instead of -removeObserver:forKeyPath: whenever possible because it allows you to more precisely specify your intent. When the same observer is registered for the same key path multiple times, but with different context pointers each time, -removeObserver:forKeyPath: has to guess at the context pointer when deciding what exactly to remove, and it can guess wrong.
+     */
     [[UserManager sharedInstance] removeObserver:self forKeyPath:@"userid" context:nil];
     [_person removeObserver:self forKeyPath:@"PIN" context:nil];
 }
@@ -55,6 +59,9 @@
         _personInfoLabel.text = [NSString stringWithFormat:@"%@'s PIN is %ld", _person.name, (long)_person.PIN];
     } else if ([keyPath isEqualToString:@"userid"] && object == [UserManager sharedInstance]) {
         _useridLabel.text = [NSString stringWithFormat:@"%@'s userid is %ld", [[UserManager sharedInstance] username], (long)[UserManager sharedInstance].userid];
+    } else {
+        // 若当前类无法捕捉到这个KVO，那很有可能他的super中有，若无此则会截断KVO，造成其super异常
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
     }
 }
 @end
